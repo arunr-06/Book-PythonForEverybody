d78d5fd12b3871b33df512d9d78c941896381664
Arun <arunkumariimr@gmail.com>
2022-11-25 20:55:40 +0530

    Codekata problems in Guvi

---
+++ Guvi.py
@@ -0,0 +1,20 @@
+# a = input()
+# n = 0
+# while n < len(a):
+#     print(a[n])
+#     n = n + 1
+
+# a = input()
+# area = 0.25*(3**0.5)*(float(a)**2)
+# b = float(area)
+# print('The value is %.2f' % b)
+
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# d = max(a, b, c)
+# print(d)
+
+a = int(input())
+b = (a * (a+1))/2
+print(int(b))
