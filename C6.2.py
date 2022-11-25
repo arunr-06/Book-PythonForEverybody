7cbb134079e931458b814ad95c20a8e2b4507134
Arun <arunkumariimr@gmail.com>
2022-11-25 13:54:26 +0530

    Chapter 6 exercise part 2

---
+++ C6.2.py
@@ -0,0 +1,34 @@
+# # string methods
+# word = input('enter the word\n')
+# letter = input('enter the letter\n')
+# a = word.count(letter)
+# print(a)
+
+# # parsing strings after @
+# email = 'arunkumariimr@gmail.com'
+# a = email.find('@')
+# print(a)
+# b = email[13:]
+# print(b)
+
+# # format operators
+# sa = 'Arun'
+# da = 6
+# aa = 45
+# fa = 7.5
+# ans = 'In %d years, %s has found %f equations' % (da, sa, fa)
+# print(ans)
+#
+# # creating a loop using function method startswith
+# while True:
+#     line = input('> ')
+#     if line.startswith('#'):
+#         continue
+#     elif line == 'done':
+#         break
+#     print(line)
+# print('Done!')
+
+
+
+
