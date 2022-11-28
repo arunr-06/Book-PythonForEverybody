6122d835e84cf6732c49ce95a49de699bc4b6482
Arun <arunkumariimr@gmail.com>
2022-11-28 10:47:15 +0530

    Chapter 7 exercise problems

---
+++ C7Q.py
@@ -0,0 +1,12 @@
+# input a file name and print the content of the file in upper
+
+# file_name = input('Enter a file name')
+# file = open(file_name)
+# for line in file:
+#     b = line.strip().upper()
+#     print(b)
+
+
+
+
+
