e2f3a7ab9e80a0fde2b86f0ca500bc558e4d3033
Arun <arunkumariimr@gmail.com>
2022-11-28 11:19:32 +0530

    Chapter 7 lesson problems

--- C7Q.py
+++ C7Q.py
@@ -6,7 +6,20 @@
 #     b = line.strip().upper()
 #     print(b)
 
-
+# #  find the average float value given the line begins with a particular string
+#
+# user_input = input('Enter the file name\n')
+# file = open(user_input, 'r')
+# count = 0
+# summation = 0
+# for line in file:
+#     if line.startswith('X-DSPAM-Confidence:'):
+#         a = line[20:]
+#         b = a.strip()
+#         count = count + 1
+#         summation = summation + float(a)
+# d = summation / count
+# print(d)
 
 
 
